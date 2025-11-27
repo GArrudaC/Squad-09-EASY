96e4420bbc043749c3dc971e9153af68a658cf61
GArrudaC <gabriel.arruda99@souunit.com.br>
2025-11-27 20:10:31 -0300

    remoção do auth baileys

--- Banco_de_dados_completo.sql
+++ Banco_de_dados_completo.sql
@@ -40,8 +40,9 @@ INSERT INTO empresas (nome, cnpj)
 VALUES ('EASY LTDA', '01.234.567/0001-89');
 
 INSERT INTO usuarios (nome, cpf, numero_whatsapp, codigo_empresa)
-VALUES ('Pedro Aurélio', '123.456.789-10', '+55 79 9692-1016', 1),
-VALUES ('Gabriel Cordeiro', '123.456.789-01', '+55 79 9106-9397', 1);
+VALUES 
+    ('Pedro Aurélio', '123.456.789-10', '+55 79 9692-1016', 1),
+    ('Gabriel Cordeiro', '123.456.789-01', '+55 79 9106-9397', 1);
 
 SHOW TABLES;
 SELECT * FROM empresas;
